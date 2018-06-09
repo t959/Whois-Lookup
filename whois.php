1a03988052baa129e91f2dea0cbd12753401d0d9
Mr.T959 <40080356+t959@users.noreply.github.com>
2018-06-09 18:13:14 +0700

    Add files via upload

---
+++ whois.php
@@ -0,0 +1,106 @@
+<head> 
+     <link href='http://fonts.googleapis.com/css?family=Federant' rel='stylesheet' type='text/css'/>
+     <link href="http://fonts.googleapis.com/css?family=Iceland" rel="stylesheet" type="text/css" />
+ <style type="text/css">
+ body {
+	   background:black; font-size:11px;
+	   font-family: Federant;
+    color: white;  }
+ a {
+     color: dodgerblue;
+     font-family: Federant;
+      }
+ a:hover {
+     border-bottom:1px solid aqua;
+      }
+ #menu a {
+ 	 font-family: Federant;
+    	padding:4px 15px;
+    	margin:0;
+    	background:darkred;
+    	color:white;
+    	text-decoration:none;
+    	letter-spacing:2px;
+    	-moz-border-radius: 5px; -webkit-border-radius: 5px; -khtml-border-radius: 5px; border-radius: 5px;
+       }
+       #menu a:hover {
+    	padding:4px 15px;
+    	margin:0;
+    	 font-family: Federant;
+    	background: grey;
+    	color:white;
+    	text-decoration:none;
+    	letter-spacing:2px;
+    	-moz-border-radius: 5px; -webkit-border-radius: 5px; -khtml-border-radius: 5px; border-radius: 5px;
+       }
+  textarea {
+  	   width:600px;
+  	   height:200px;
+  	   background: black;
+  	   border:1px solid darkred;
+  	   color: white;
+  	   font-family: Federant;
+  	   }
+  input[type=text] , input[type=file] , select {   
+       background:black;
+       color:white;border: 1px solid darkred; 
+       padding:6px 6px 6px 6px;
+       font-family: Federant;
+        }
+  input[type=submit] {
+       background:#b70505;
+       color:white;border: 1px solid #000; 
+       padding:6px 6px 6px 6px;
+       font-family: Federant;
+       }
+  button[type=submit] {
+       background:#b70505;
+       color:white;border: 1px solid #000; 
+       padding:6px 6px 6px 6px;
+       font-family: Federant;
+       }
+  .subbtn:hover {
+  	   background:#c0bfbf;
+  	   color:#000000;
+  	   font-family: Federant;
+  	   }
+
+td, th { font-size: 12pt; text-align: left; vertical-align: top; color: dodgerblue; }
+h1           { font-size: 16pt; text-align: center; }
+h1 a         { color: #000000 !important; text-decoration: none; }
+p            { text-align: center; font-size: 9pt; }
+p a          { color: #666666 !important; }
+table        {  margin: 0 auto; border-collapse: collapse; border: 1px solid #ffffff; min-width: 400px; }
+th, td       { padding: 5px 10px; }
+th           { background: black; color: #ffffff; }
+td a         { color: dodgerblue !important; text-decoration: none; }
+th img       { position: relative; top: -3px; left: 2px; }
+td           { border-bottom: 1px solid #cccccc; background: black; }
+tr.odd td    { background: black; }
+
+#lol a {
+    	padding:4px 15px;
+    	margin:0;
+    	background:darkgreen;
+    	color:white;
+    	text-decoration:none;
+    	letter-spacing:2px;
+    	-moz-border-radius: 5px; -webkit-border-radius: 5px; -khtml-border-radius: 5px; border-radius: 5px;
+       }
+	  
+</style>
+
+<title>Whois Lookup</title>
+<br />
+        <center> <font color="red" face="iceland" size="10">Whois Lookup</font>
+		 <br /> <br />
+<img src="https://cdn.pbrd.co/images/6RXeCi5n8.png" width="100" height="100">
+<br /> <br />
+
+		<form action="" id="form1" method="get" onsubmit=" location.href = 'https://www.whois.com/whois/' + document.getElementById('url').value; return false; "> 
+	
+			<input type="text" name="url" value="mr-t959.xyz" id="url"/> 
+	  
+		</form> 
+		
+		<button type="submit" form="form1" value="Submit">Submit</button>
